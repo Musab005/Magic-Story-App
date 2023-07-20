af91cd9f1bf8f2cc7e9f20e6e07221f02ccb464d
Musab Umair <musabumair005@gmail.com>
2023-07-20 18:43:13 +0500

    fix LandingPage UI

--- app/src/main/java/com/apps005/magicstory/View/LandingPage.java
+++ app/src/main/java/com/apps005/magicstory/View/LandingPage.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.apps005.magicstory.R;
 import com.apps005.magicstory.databinding.ActivityLandingPageBinding;
@@ -11,10 +14,17 @@ import com.apps005.magicstory.databinding.ActivityLandingPageBinding;
 public class LandingPage extends AppCompatActivity {
 
     private ActivityLandingPageBinding bo;
+    private Button save_button;
+    private TextView txtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bo = DataBindingUtil.setContentView(this, R.layout.activity_landing_page);
+        save_button = bo.saveButton;
+        txtView = bo.usernameGuide;
+        save_button.setOnClickListener(view -> txtView.setText("hello"));
+
+
     }
 }
